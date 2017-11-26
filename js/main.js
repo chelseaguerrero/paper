6a42e8be028a805c4cb520bb0244889609cd838c
chelseaguerrero <32784455+chelseaguerrero@users.noreply.github.com>
2017-11-25 18:23:06 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,627 @@
+//Tray Steps Mouseout
+$(document).ready(function(){
+  $("p.tc1").mouseout(function() {
+    $("img.tsteps1").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc2").mouseout(function() {
+    $("img.tsteps2").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc3").mouseout(function() {
+    $("img.tsteps3").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc4").mouseout(function() {
+    $("img.tsteps4").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc5").mouseout(function() {
+    $("div.tsteps5").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc6").mouseout(function() {
+    $("img.tsteps6").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc7").mouseout(function() {
+    $("img.tsteps7").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc8").mouseout(function() {
+    $("img.tsteps7").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc9").mouseout(function() {
+    $("img.tsteps9").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc10").mouseout(function() {
+    $("img.tsteps10").css({
+     "z-index": "1"
+    });
+  });
+});
+
+
+
+//Tray Steps Mouseover
+$(document).ready(function(){
+  $("p.tc1").mouseover(function() {
+    $("img.tsteps1").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc2, p.hbullet").mouseover(function() {
+    $("img.tsteps2").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc3").mouseover(function() {
+    $("img.tsteps3").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc4").mouseover(function() {
+    $("img.tsteps4").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc5").mouseover(function() {
+    $("div.tsteps5").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc6").mouseover(function() {
+    $("img.tsteps6").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc7").mouseover(function() {
+    $("img.tsteps7").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc8").mouseover(function() {
+    $("img.tsteps7").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc9").mouseover(function() {
+    $("img.tsteps9").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.tc10").mouseover(function() {
+    $("img.tsteps10").css({
+     "z-index": "10"
+    });
+  });
+});
+
+
+
+//Boat Steps Mouseout
+$(document).ready(function(){
+  $("p.bc1, p.bbullet").mouseout(function() {
+    $("img.bsteps1").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc2").mouseout(function() {
+    $("img.bsteps2").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc3").mouseout(function() {
+    $("img.bsteps3").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc4").mouseout(function() {
+    $("img.bsteps4").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc5").mouseout(function() {
+    $("img.bsteps5").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc6").mouseout(function() {
+    $("img.bsteps6").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc7").mouseout(function() {
+    $("img.bsteps7").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc8").mouseout(function() {
+    $("img.bsteps8").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc9").mouseout(function() {
+    $("img.bsteps9").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc10").mouseout(function() {
+    $("img.bsteps10").css({
+     "z-index": "1"
+    });
+  });
+});
+
+
+
+
+//Boat Steps Mouseover
+$(document).ready(function(){
+  $("p.bc1, p.bbullet").mouseover(function() {
+    $("img.bsteps1").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc2").mouseover(function() {
+    $("img.bsteps2").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc3").mouseover(function() {
+    $("img.bsteps3").css({
+     "z-index": "10"
+    });
+  });
+});
+
+
+$(document).ready(function(){
+  $("p.bc4").mouseover(function() {
+    $("img.bsteps4").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc5").mouseover(function() {
+    $("img.bsteps5").css({
+     "z-index": "10"
+    });
+  });
+});
+
+
+$(document).ready(function(){
+  $("p.bc6").mouseover(function() {
+    $("img.bsteps6").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc7").mouseover(function() {
+    $("img.bsteps7").css({
+     "z-index": "10"
+    });
+  });
+});
+
+
+$(document).ready(function(){
+  $("p.bc8").mouseover(function() {
+    $("img.bsteps8").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.bc9").mouseover(function() {
+    $("img.bsteps9").css({
+     "z-index": "10"
+    });
+  });
+});
+
+
+$(document).ready(function(){
+  $("p.bc10").mouseover(function() {
+    $("img.bsteps10").css({
+     "z-index": "10"
+    });
+  });
+});
+
+
+
+
+//Hat Steps Mouseout
+$(document).ready(function(){
+  $("p.hc1").mouseout(function() {
+    $("img.hsteps1").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc2, p.hbullet").mouseout(function() {
+    $("img.hsteps2").css({
+     "z-index": "2"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc3").mouseout(function() {
+    $("img.hsteps3").css({
+     "z-index": "3"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc4").mouseout(function() {
+    $("img.hsteps4").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc5").mouseout(function() {
+    $("img.hsteps5").css({
+     "z-index": "2"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc6").mouseout(function() {
+    $("img.hsteps6").css({
+     "z-index": "3"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc7").mouseout(function() {
+    $("img.hsteps7").css({
+     "z-index": "1"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc8").mouseout(function() {
+    $("img.hsteps8").css({
+     "z-index": "2"
+    });
+  });
+});
+
+
+
+//Hat Steps Mouseover
+$(document).ready(function(){
+  $("p.hc1").mouseover(function() {
+    $("img.hsteps1").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc2, p.hbullet").mouseover(function() {
+    $("img.hsteps2").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc3").mouseover(function() {
+    $("img.hsteps3").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc4").mouseover(function() {
+    $("img.hsteps4").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc5").mouseover(function() {
+    $("img.hsteps5").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc6").mouseover(function() {
+    $("img.hsteps6").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc7").mouseover(function() {
+    $("img.hsteps7").css({
+     "z-index": "10"
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p.hc8").mouseover(function() {
+    $("img.hsteps8").css({
+     "z-index": "10"
+    });
+  });
+});
+
+
+
+
+// Material Circle 1
+$ (window).scroll(function(){
+   var fromthetop = $(window).scrollTop();
+    if (fromthetop > 899) {
+        $ ("img.matcircle1").fadeIn();
+       }
+     else {
+      $ ("img.matcircle1").fadeOut();
+     }});
+
+
+
+// Material Info
+$ (window).scroll(function(){
+   var fromthetop = $(window).scrollTop();
+    if (fromthetop > 899) {
+        $ ("p.mattext").fadeIn();
+       }
+     else {
+      $ ("p.mattext").fadeOut();
+    }});
+
+
+
+//About Icon
+$ (window).scroll(function(){
+   var fromthetop = $(window).scrollTop();
+    if (fromthetop > 5390) {
+        $ ("img.abtcircle0").css({
+       "position": "fixed",
+       "top": "310px",
+       "width": "40px",
+       "height": "40px"
+       });}
+     else {
+      $ ("img.abtcircle0").css({
+       "position": "absolute",
+       "top": "5700px"
+      });}
+   });
+
+
+
+//Hooray Icon
+$ (window).scroll(function(){
+   var fromthetop = $(window).scrollTop();
+    if (fromthetop > 4650) {
+        $ ("img.hrcircle0").css({
+       "position": "fixed",
+       "top": "260px",
+       "width": "40px",
+       "height": "40px"
+       });}
+     else {
+      $ ("img.hrcircle0").css({
+       "position": "absolute",
+       "top": "4900px"
+      });}
+   });
+
+
+
+//Tray Icon
+$ (window).scroll(function(){
+   var fromthetop = $(window).scrollTop();
+    if (fromthetop > 3750) {
+        $ ("img.traycircle0").css({
+       "position": "fixed",
+       "top": "210px",
+       "width": "40px",
+       "height": "40px"
+      });}
+     else {
+      $ ("img.traycircle0").css({
+       "position": "absolute",
+       "top": "4000px"
+      });}
+   });
+
+
+
+//Boat Icon
+$ (window).scroll(function(){
+   var fromthetop = $(window).scrollTop();
+    if (fromthetop > 2440) {
+        $ ("img.boatcircle0").css({
+       "position": "fixed",
+       "top": "160px",
+       "width": "40px",
+       "height": "40px"
+       });}
+     else {
+      $ ("img.boatcircle0").css({
+       "position": "absolute",
+       "top": "2600px"
+      });}
+   });
+
+
+
+//Hat Icon
+$ (window).scroll(function(){
+   var fromthetop = $(window).scrollTop();
+    if (fromthetop > 1580) {
+        $ ("img.hatcircle0").css({
+       "position": "fixed",
+       "top": "110px",
+       "width": "40px",
+       "height": "40px"
+       });}
+     else {
+      $ ("img.hatcircle0").css({
+       "position": "absolute",
+       "top": "1700px"
+      });}
+   });
+
+
+
+ //Materials Icon
+$ (window).scroll(function(){
+   var fromthetop = $(window).scrollTop();
+    if (fromthetop > 960) {
+        $ ("img.matcircle0").css({
+       "position": "fixed",
+       "top": "60px",
+       "width": "40px",
+       "height": "40px"
+       });}
+     else {
+      $ ("img.matcircle0").css({
+       "position": "absolute",
+       "top": "1030px"
+      });}
+   });
+
+
+
+// HI Icon
+$ (window).scroll(function(){
+   var fromthetop = $(window).scrollTop();
+    if (fromthetop > 480) {
+        $ ("div.hi").css({
+       "position": "fixed",
+       "top": "10px",
+       "width": "40px",
+       "height": "40px"
+       });}
+     else {
+      $ ("div.hi").css({
+       "position": "absolute",
+       "top": "50%"
+      });}
+   });
